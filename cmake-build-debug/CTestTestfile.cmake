225742c54876c4518ef5a2be4251479b11f16841
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2021-03-15 14:32:44 +0300

    Fix

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -4,3 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
+add_test(test_main.out "test_main.out")
+set_tests_properties(test_main.out PROPERTIES  _BACKTRACE_TRIPLES "/Users/ivanchernov/CLionProjects/DZ1/CMakeLists.txt;30;add_test;/Users/ivanchernov/CLionProjects/DZ1/CMakeLists.txt;0;")
--- src/test_main.cpp
+++ src/test_main.cpp
@@ -8,7 +8,7 @@ extern "C" {
 // просто проверка на то что все подтягивает и в конце ничего не добавляет тк память не выделена
 TEST(TEST_HTML_TAG, start_tag) {
     char str[SIZE_VALUE] = R"(<form  action="/questions/511196/answer/submit" class="js-add-answer-component post-form">\n)";
-    html * tag = html_decoder(str);
+    html * tag = (html *) html_decoder(str);
     if (strcmp(tag[0].name, "form") != 0)
         GTEST_FAIL() << "Wrong answer";
     if (strcmp(tag[0].value, "open") != 0)
@@ -21,16 +21,12 @@ TEST(TEST_HTML_TAG, start_tag) {
         GTEST_FAIL() << " Wrong answer";
     if (strcmp(tag[2].value, "js-add-answer-component post-form") != 0)
         GTEST_FAIL() << "Wrong answer";
-    int q = 0;
-    while (q < 3) {
-        free(tag + q); q++;
-    }
 }
 
 // проверка на пробелл перед тегом (такой тег не валиден)
 TEST(TEST_HTML_TAG, error_start_tag) {
     char str[SIZE_VALUE] = R"(< form class="js-add-answer-component post-form">\n)";
-    html * tag = html_decoder(str);
+    html * tag = (html *) html_decoder(str);
     if (strcmp(tag->name, "0") != 0)
         GTEST_FAIL() << "Incorrect name tag";
     EXPECT_EQ(tag->name[0], '0');
@@ -39,7 +35,7 @@ TEST(TEST_HTML_TAG, error_start_tag) {
 // проверка на то что он закрывающий (close) и в name не хранит символ "/"
 TEST(TEST_HTML_TAG, close_tag) {
     char str[SIZE_VALUE] = R"(</form>\n)";
-    html * tag = html_decoder(str);
+    html * tag = (html *) html_decoder(str);
     if (strcmp(tag->name, "form") != 0)
         GTEST_FAIL() << " Wrong answer";
     if (strcmp(tag->value, "close") != 0)
@@ -48,7 +44,7 @@ TEST(TEST_HTML_TAG, close_tag) {
 // проверка на отступ после "/" в случае закрывающего тега
 TEST(TEST_HTML_TAG, error_close_tag) {
     char str[SIZE_VALUE] = R"(</ form>\n)";
-    html * tag = html_decoder(str);
+    html * tag = (html *) html_decoder(str);
     if (strcmp(tag->name, "0") != 0)
         GTEST_FAIL() << "Incorrect name tag";
     EXPECT_EQ(tag->name[0], '0');
@@ -57,7 +53,7 @@ TEST(TEST_HTML_TAG, error_close_tag) {
 // проверка на правильный нейминг атрибутов тега, никаких символов (только в value атрибута)
 TEST(TEST_HTML_TAG, error_attribute_tag) {
     char str[SIZE_VALUE] = R"(<form c);l"a;ss="js-add-answer-component post-form">\n)";
-    html * tag = html_decoder(str);
+    html * tag = (html *) html_decoder(str);
     if (strcmp(tag->name, "0") != 0)
         GTEST_FAIL() << "Incorrect name tag";
     EXPECT_EQ(tag->name[0], '0');
@@ -66,7 +62,7 @@ TEST(TEST_HTML_TAG, error_attribute_tag) {
 // проверка на то как он читает пробеллы (дышащий тег)
 TEST(TEST_HTML_TAG, spaces_in_tag) {
     char str[SIZE_VALUE] = R"(<form   action =  " /questions/511196/answer/submit " >\n)";
-    html * tag = html_decoder(str);
+    html * tag = (html *) html_decoder(str);
     if (strcmp(tag[0].name, "form") != 0)
         GTEST_FAIL() << "Wrong answer";
     if (strcmp(tag[0].value, "open") != 0)
@@ -82,7 +78,7 @@ TEST(TEST_HTML_TAG, spaces_in_tag) {
 // проверка на то что тег содержит "<"
 TEST(TEST_HTML_TAG,  error_start_str_tag) {
     char str[SIZE_VALUE] = R"(  form   action="/questions/">\n)";
-    html * tag = html_decoder(str);
+    html * tag = (html *) html_decoder(str);
     if (strcmp(tag->name, "0") != 0)
         GTEST_FAIL() << "Incorrect name tag";
     EXPECT_EQ(tag->name[0], '0');
@@ -91,7 +87,7 @@ TEST(TEST_HTML_TAG,  error_start_str_tag) {
 // проверка на то что тег содержит ">"
 TEST(TEST_HTML_TAG,  error_end_str_tag) {
     char str[SIZE_VALUE] = R"( <form   action="/questions/"\n)";
-    html * tag = html_decoder(str);
+    html * tag = (html *) html_decoder(str);
     if (strcmp(tag->name, "0") != 0)
         GTEST_FAIL() << "Incorrect name tag";
     EXPECT_EQ(tag->name[0], '0');
@@ -100,7 +96,7 @@ TEST(TEST_HTML_TAG,  error_end_str_tag) {
 // проверка на то что он пустой
 TEST(TEST_HTML_TAG,  null_str_tag) {
     char str[SIZE_VALUE] = R"( )";
-    html * tag = html_decoder(str);
+    html * tag = (html *) html_decoder(str);
     if (strcmp(tag->name, "0") != 0)
         GTEST_FAIL() << "IS NULL";
     EXPECT_EQ(tag->name[0], '0');
@@ -109,7 +105,7 @@ TEST(TEST_HTML_TAG,  null_str_tag) {
 // Есть особая форма атрибута без кавычек (action=/search) и не такое бывает
 TEST(TEST_HTML_TAG,  spechial_attr_tag) {
     char str[SIZE_VALUE] = R"(<form  action=/search class="post-form">\n)";
-    html * tag = html_decoder(str);
+    html * tag = (html *) html_decoder(str);
     if (strcmp(tag[0].name, "form") != 0)
         GTEST_FAIL() << "Wrong answer";
     if (strcmp(tag[0].value, "open") != 0)
