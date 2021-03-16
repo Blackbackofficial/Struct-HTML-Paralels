fb6fa55287b77c36ddeddad7e4b5313425e84fdd
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2021-03-16 04:13:18 +0300

    Clean code

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ivanchernov/CLionProjects/DZ1
-# Build directory: /Users/ivanchernov/CLionProjects/DZ1/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_main.out "test_main.out")
-set_tests_properties(test_main.out PROPERTIES  _BACKTRACE_TRIPLES "/Users/ivanchernov/CLionProjects/DZ1/CMakeLists.txt;30;add_test;/Users/ivanchernov/CLionProjects/DZ1/CMakeLists.txt;0;")
